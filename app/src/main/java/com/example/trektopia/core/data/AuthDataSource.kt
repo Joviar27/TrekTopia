81ca7403b0a9a4d8be08a6608618dee8b2c7b941
Joviar <joviar27@gmail.com>
2023-11-17 04:20:35 +0700

    Create authentication data source

--- app/src/main/java/com/example/trektopia/core/data/AuthDataSource.kt
+++ app/src/main/java/com/example/trektopia/core/data/AuthDataSource.kt
@@ -3,52 +3,54 @@ package com.example.trektopia.core.data
 import android.util.Log
 import com.example.trektopia.core.AuthState
 import com.example.trektopia.core.ResultState
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.asDeferred
-import kotlinx.coroutines.tasks.await
 
 class AuthDataSource(
     private val auth: FirebaseAuth
 ){
-
-    suspend fun signUp(email: String, password: String): Flow<ResultState<String>> = callbackFlow {
+    fun signUp(
+        email: String,
+        password: String
+    ): Flow<ResultState<String>> = callbackFlow {
+        trySend(ResultState.Loading)
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d("AuthRepository", "Sign Up Success")
-                    task.result.user.let { user ->
-                        if(user!=null) trySend(ResultState.Success(user.uid))
-                    }
-                } else {
-                    Log.d("AuthRepository", "SignUp: ${task.exception?.message.toString()}")
-                    trySend(ResultState.Error(task.exception?.message.toString()))
+                val user = task.result.user
+                if(user != null && task.isSuccessful){
+                    Log.e("AuthDataSource", "Sign Up Success")
+                    trySend(ResultState.Success(user.uid))
+                }
+                else{
+                    val exception = task.exception?.message ?: "User is Null"
+                    Log.e("AuthDataSource", "SignUp: $exception")
+                    trySend(ResultState.Error(exception))
                 }
                 close()
             }
         awaitClose()
     }
 
-    suspend fun signIn(email: String, password: String): Flow<ResultState<String>> = callbackFlow {
+    fun signIn(
+        email: String,
+        password: String
+    ): Flow<ResultState<String>> = callbackFlow {
+        trySend(ResultState.Loading)
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener{ task ->
-                if (task.isSuccessful) {
-                    Log.d("AuthRepository", "Sign In Success")
-                    task.result.user.let { user ->
-                        if(user!=null) trySend(ResultState.Success(user.uid))
-                    }
-                } else {
-                    Log.d("AuthRepository", "SignIn: ${task.exception?.message.toString()}")
-                    trySend(ResultState.Error(task.exception?.message.toString()))
+                val user = task.result.user
+                if(user != null && task.isSuccessful){
+                    Log.e("AuthDataSource", "Sign In Success")
+                    trySend(ResultState.Success(user.uid))
+                }
+                else{
+                    val exception = task.exception?.message ?: "User is Null"
+                    Log.e("AuthDataSource", "SignIn: $exception")
+                    trySend(ResultState.Error(exception))
                 }
                 close()
             }
@@ -57,7 +59,7 @@ class AuthDataSource(
 
     fun logout() = auth.signOut()
 
-    suspend fun getAuthState(): Flow<AuthState> = callbackFlow {
+    fun getAuthState(): Flow<AuthState> = callbackFlow {
         val authStateListener = AuthStateListener { auth ->
             if(auth.currentUser != null){
                 trySend(AuthState.Authenticated)
@@ -70,7 +72,7 @@ class AuthDataSource(
         awaitClose { auth.removeAuthStateListener(authStateListener) }
     }
 
-    suspend fun getUid(): Flow<String?> = flow{
+    fun getUid(): Flow<String?> = flow{
         emit(auth.currentUser?.uid)
     }
 
