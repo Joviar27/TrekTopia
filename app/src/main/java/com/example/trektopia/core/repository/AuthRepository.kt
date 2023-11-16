e78f52ab13876a0452db705f7616a122d5dbdb0c
Joviar <joviar27@gmail.com>
2023-11-17 04:21:27 +0700

    Create authentication repository

--- app/src/main/java/com/example/trektopia/core/repository/AuthRepository.kt
+++ app/src/main/java/com/example/trektopia/core/repository/AuthRepository.kt
@@ -1,49 +1,37 @@
 package com.example.trektopia.core.repository
 
-import android.util.Log
 import com.example.trektopia.core.AuthState
 import com.example.trektopia.core.ResultState
 import com.example.trektopia.core.data.AuthDataSource
 import com.example.trektopia.core.data.FirestoreDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlin.Exception
 
 class AuthRepository(
     private val authDataSource: AuthDataSource,
     private val firestoreDataSource: FirestoreDataSource
 ) {
-
-    fun signIn(email: String, password: String): Flow<ResultState<String>> = flow{
-        emit(ResultState.Loading)
+    fun signIn(email: String, password: String): Flow<ResultState<String>> =
         authDataSource.signIn(email, password)
-    }
 
-    suspend fun getAuthState(): Flow<AuthState> = authDataSource.getAuthState()
+    fun getAuthState(): Flow<AuthState> = authDataSource.getAuthState()
 
-    suspend fun getUid(): Flow<String?> = authDataSource.getUid()
+    fun getUid(): Flow<String?> = authDataSource.getUid()
 
     fun logout() = authDataSource.logout()
 
-
-    suspend fun signUp(username: String, email: String, password: String): Flow<ResultState<String>> = flow{
-        emit(ResultState.Loading)
+    fun signUp(
+        username: String,
+        email: String,
+        password: String
+    ): Flow<ResultState<String>> = flow{
         authDataSource.signUp(email,password).map { result ->
             when(result) {
                 is ResultState.Success -> {
-                    try {
-                        firestoreDataSource.insertNewUser(uid = result.data, username, email)
-
-                        firestoreDataSource.assignAllMissions(userId = result.data)
-
-                        firestoreDataSource.assignAllAchievements(userId = result.data)
-
-                        emit(result)
-                    } catch (e: Exception) {
-                        Log.d("AuthRepository", "signUp: ${e.message.toString()}")
-                        emit(ResultState.Error(e.message.toString()))
-                    }
+                    val setupResult = firestoreDataSource.setupUser(result.data, username, email)
+                    emitAll(setupResult)
                 }
                 else -> emit(result)
             }
