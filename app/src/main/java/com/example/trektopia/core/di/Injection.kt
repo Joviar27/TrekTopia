36fd3b0ded591db40c0ef106ae68eb7309147676
Joviar <joviar27@gmail.com>
2023-11-23 07:54:05 +0700

    Create dependency injection

--- app/src/main/java/com/example/trektopia/core/di/Injection.kt
+++ app/src/main/java/com/example/trektopia/core/di/Injection.kt
@@ -1,4 +1,37 @@
 package com.example.trektopia.core.di
 
+import com.example.trektopia.core.data.AuthDataSource
+import com.example.trektopia.core.data.FirestoreDataSource
+import com.example.trektopia.core.data.StorageDataSource
+import com.example.trektopia.core.repository.AuthRepository
+import com.example.trektopia.core.repository.GameRepository
+import com.example.trektopia.core.repository.Repository
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
 object Injection {
+
+    private fun provideAuthDataSource() = AuthDataSource(Firebase.auth)
+    private fun provideFirestoreDataSource() = FirestoreDataSource(Firebase.firestore)
+    private fun provideStorageDataSource() = StorageDataSource(Firebase.storage.reference)
+
+    fun provideAuthRepository() =
+        AuthRepository(
+            provideAuthDataSource(),
+            provideFirestoreDataSource()
+        )
+
+    fun provideGameRepository() =
+        GameRepository(
+            provideFirestoreDataSource()
+        )
+
+    fun provideRepository() =
+        Repository(
+            provideFirestoreDataSource(),
+            provideStorageDataSource()
+        )
+
 }
