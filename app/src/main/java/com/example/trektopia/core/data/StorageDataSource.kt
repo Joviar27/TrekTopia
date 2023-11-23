c06ce492bbd1e539ecbeeed6fc3b9cdef2663997
Joviar <joviar27@gmail.com>
2023-11-23 07:55:28 +0700

    Add update profile picture function

--- app/src/main/java/com/example/trektopia/core/data/StorageDataSource.kt
+++ app/src/main/java/com/example/trektopia/core/data/StorageDataSource.kt
@@ -1,55 +1,32 @@
 package com.example.trektopia.core.data
 
 import android.net.Uri
-import android.util.Log
-import com.example.trektopia.core.ResultState
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
-import java.lang.Exception
 
 class StorageDataSource(
     private val storage: StorageReference,
-    private val db: FirebaseFirestore
 ) {
-    fun updateProfile(
-        imageUri : Uri,
-        userId : String
-    ): Flow<ResultState<Unit>> =  flow{
-        emit(ResultState.Loading)
-        try {
-            val profileRef = storage.child(userId)
-            val oldUri = profileRef.downloadUrl.await()
 
-            val uploadTask = profileRef.putFile(imageUri)
-            val newUri = uploadTask.continueWithTask{ task ->
-                if(!task.isSuccessful){
-                    task.exception?.let { throw it }
-                }
-                profileRef.downloadUrl
-            }.await()
-
-            updatePictureUri(userId, profileRef, newUri, oldUri)
-            emit(ResultState.Success(Unit))
-        } catch (e: Exception){
-            emit(ResultState.Error(e.message.toString()))
-            Log.e("StorageDataSource", "updateProfile: $e")
-        }
-    }
-
-    private fun updatePictureUri(
+    suspend fun getDownloadUrl(
         userId: String,
-        profileRef: StorageReference,
-        newUri: Uri,
-        oldUri: Uri
-    ){
-        db.collection("users").document(userId).update("pictureUri",newUri)
-            .addOnFailureListener{e ->
-                profileRef.putFile(oldUri)
-                Log.e("StorageDataSource", "updatePictureUri: $e")
+        imageUri: Uri
+    ):Uri {
+        val newUri = uploadProfile(userId, imageUri).continueWithTask{ task ->
+            if(!task.isSuccessful){
+                task.exception?.let { throw it }
             }
+            storage.child(userId).downloadUrl
+        }.await()
+        return newUri
     }
+
+    suspend fun getProfile(userId: String) : Uri =
+        storage.child(userId).downloadUrl.await()
+
+    fun uploadProfile(userId: String, uri: Uri) =
+        storage.child(userId).putFile(uri)
+
+
 
 }
--- app/src/main/java/com/example/trektopia/core/repository/Repository.kt
+++ app/src/main/java/com/example/trektopia/core/repository/Repository.kt
@@ -1,120 +1,62 @@
 package com.example.trektopia.core.repository
 
 import android.net.Uri
+import android.util.Log
 import com.example.trektopia.core.ResultState
 import com.example.trektopia.core.data.FirestoreDataSource
 import com.example.trektopia.core.data.StorageDataSource
 import com.example.trektopia.core.model.Activity
-import com.example.trektopia.core.model.Relation
-import com.example.trektopia.core.model.Task
-import com.example.trektopia.core.model.TaskProgress
-import com.example.trektopia.core.model.TaskType
 import com.example.trektopia.core.model.User
-import com.google.firebase.firestore.CollectionReference
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class Repository(
-    private val firestoreDataSource: FirestoreDataSource,
-    private val storageDataSource: StorageDataSource
+    private val firestore: FirestoreDataSource,
+    private val storage: StorageDataSource
 ) {
-    suspend fun getUserData(userId: String) = firestoreDataSource.getUserData(userId)
-
-    private suspend fun getTaskData(taskCollectionReference: CollectionReference) =
-        firestoreDataSource.getTaskData(taskCollectionReference)
-
-    private suspend fun getRelationData(userId: String, relationCollectionReference: CollectionReference) =
-        firestoreDataSource.getRelationData(userId, relationCollectionReference)
-
-    fun resetDailyMission() = firestoreDataSource.resetDailyMission()
-
-    fun resetStreak(userId: String) = firestoreDataSource.resetStreak(userId)
+    fun getUserData(userId: String) = firestore.getUserData(userId)
 
     //To show notification if user haven't done any activity
-    fun checkLatestActiveDate(userId: String) = firestoreDataSource.checkLatestActiveDate(userId)
+    fun checkLatestActiveDate(userId: String) = firestore.checkLatestActiveDate(userId)
 
-    fun getLeaderboard() = firestoreDataSource.getLeaderboard()
-
-    //Might delete later if not effective
-    fun getUserRank(userPoint: Int) = firestoreDataSource.getUserRank(userPoint)
-
-    fun getUserActivities(userId: String) = firestoreDataSource.getUserActivities(userId)
+    fun getUserActivities(userId: String) = firestore.getUserActivities(userId)
 
     fun updateUserInfo(userId: String, newUser: User) =
-        firestoreDataSource.updateUserInfo(userId,newUser)
-
-    fun updateProfile(imageUri: Uri, userId: String) =
-        storageDataSource.updateProfile(imageUri,userId)
-
-    fun claimTaskReward(userId: String, relationId: String, taskId: String, taskType: TaskType) =
-        firestoreDataSource.claimTaskReward(userId, relationId, taskId, taskType)
+        firestore.updateUserInfo(userId,newUser)
 
     fun addActivityAndUpdateProgress(userId: String, activity: Activity) =
-        firestoreDataSource.addActivityAndUpdateProgress(activity, userId)
-    suspend fun getTaskWithProgress(
-        userId: String,
-        taskType: TaskType
-    ): Flow<ResultState<List<TaskProgress>>> {
-        val taskCollectionRef = firestoreDataSource.getTaskCollectionRef(taskType)
-        val relationCollectionRef = firestoreDataSource.getRelationCollectionRef(taskType)
+        firestore.addActivityAndUpdateProgress(activity, userId)
 
-        val relationsFlow = getRelationData(userId, relationCollectionRef)
-        val taskFlow = getTaskData(taskCollectionRef)
+    private suspend fun getProfile(userId: String) = storage.getProfile(userId)
 
-        return relationsFlow.combine(taskFlow) { listRelation, listMission ->
-            combineResult(listRelation, listMission){ relation, task ->
-                mapToTaskRelation(relation,task)
+    private suspend fun getDownloadUrl(userId: String, imageUri: Uri) =
+        storage.getDownloadUrl(userId, imageUri)
+
+    private fun uploadProfile(userId: String, imageUri: Uri) =
+        storage.uploadProfile(userId, imageUri)
+
+    private fun updatePictureUri(userId: String, imageUri: Uri) =
+        firestore.updatePictureUri(userId, imageUri)
+
+    fun updateProfilePicture(
+        imageUri: Uri,
+        userId: String
+    ): Flow<ResultState<Unit>> = flow{
+        emit(ResultState.Loading)
+        try {
+            val oldUri = getProfile(userId)
+            val newUri = getDownloadUrl(userId, imageUri)
+
+            updatePictureUri(userId, newUri).map { result ->
+                when(result){
+                    is ResultState.Error -> uploadProfile(userId, oldUri)
+                    else -> emit(result)
+                }
             }
+        } catch (e: Exception){
+            emit(ResultState.Error(e.message.toString()))
+            Log.e("FirestoreDataSource", "updateProfilePicture: $e")
         }
-    }
-
-    private inline fun <R,T,M> combineResult(
-        result1: ResultState<R>,
-        result2: ResultState<T>,
-        combine: (ResultState<R>, ResultState<T>) -> ResultState<M>
-    ): ResultState<M>{
-        return when {
-            result1 is ResultState.Error -> ResultState.Error(result1.error)
-            result2 is ResultState.Error -> ResultState.Error(result2.error)
-            result1 is ResultState.Loading || result2 is ResultState.Loading -> ResultState.Loading
-            else -> {
-                combine(result1,result2)
-            }
-        }
-    }
-
-    private fun mapToTaskRelation(
-        userMissions: ResultState<List<Pair<String,Relation>>>,
-        allMissions: ResultState<List<Task>>
-    ): ResultState<List<TaskProgress>> {
-        val relations = (userMissions as ResultState.Success).data
-        val taskList = (allMissions as ResultState.Success).data
-
-        val taskIdMap = taskList.associateBy { it.id }
-
-        val taskProgress = relations.mapNotNull { relation ->
-            val task = taskIdMap[relation.second.taskRef]
-            if (task != null) {
-                TaskProgress(
-                    task = task,
-                    progress = relation.second.progress,
-                    relationId = relation.first
-                )
-            } else null
-        }
-
-        return if(taskProgress.size < relations.size || taskProgress.size < taskList.size)
-            ResultState.Error("Some taskProgress is mission")
-        else ResultState.Success(taskProgress)
     }
 }
-
-/*
-suspend fun getMissionsData(): Flow<List<DailyMission>> =
-    firestoreDataSource.getMissionsData()
- */
-
-/*
-suspend fun getUserMissionRelation(userId: String): Flow<List<UserMissionRelation>> =
-    firestoreDataSource.getUserMissionRelation(userId)
- */
