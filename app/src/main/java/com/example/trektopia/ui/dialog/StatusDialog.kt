f36adfc3f8926e3cdb045ea17b81c7652f41ff60
Joviar <joviar27@gmail.com>
2023-12-01 07:33:31 +0700

    Create status dialog

---
+++ app/src/main/java/com/example/trektopia/ui/dialog/StatusDialog.kt
@@ -0,0 +1,54 @@
+package com.example.trektopia.ui.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.DialogFragment
+import com.example.trektopia.databinding.DialogStatusBinding
+
+class StatusDialog : DialogFragment() {
+
+    private lateinit var binding: DialogStatusBinding
+
+    companion object {
+        private const val ARG_ICON = "icon"
+        private const val ARG_MESSAGE = "message"
+
+        fun newInstance(icon: Int, message: String): StatusDialog {
+            val fragment = StatusDialog()
+            val args = Bundle().apply {
+                putInt(ARG_ICON, icon)
+                putString(ARG_MESSAGE, message)
+            }
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        var icon = 0
+        var message =""
+        if(arguments!=null){
+            icon = arguments?.getInt(ARG_ICON) ?: 0
+            message = arguments?.getString(ARG_ICON) ?: "Dialog Error"
+        }
+
+        binding = DialogStatusBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.apply {
+            ivIcon.setImageDrawable(ResourcesCompat.getDrawable(resources,icon,null))
+            tvMessage.text = message
+        }
+
+        return AlertDialog.Builder(requireActivity())
+            .setView(view)
+            .setTitle("Please wait..")
+            .create()
+    }
+
+    fun closeDialog(){
+        dismiss()
+    }
+}
