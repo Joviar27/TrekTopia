971fa65160e5d992bba97f96a8407f11be2a935f
Joviar <joviar27@gmail.com>
2023-12-09 20:33:33 +0700

    Add delay

--- app/src/main/java/com/example/trektopia/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/trektopia/ui/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.example.trektopia.ui.splash
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import com.example.trektopia.R
@@ -12,6 +15,8 @@ import com.example.trektopia.core.AuthState
 import com.example.trektopia.utils.obtainViewModel
 import com.example.trektopia.utils.safeNavigate
 import com.example.trektopia.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
@@ -28,6 +33,8 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //TODO: Put some delay action
+        lifecycleScope.launch {
+            delay(5000)
+        }
     }
 }
