6e836c6478c53fe25e4e1dc3903aaff0a42fd87b
Joviar <joviar27@gmail.com>
2023-12-09 20:37:01 +0700

    Update utils
    (cherry picked from commit 063078b8141bd4f35f3bf13f92c2ee658a8f9e69)

--- app/src/main/java/com/example/trektopia/utils/Polyline.kt
+++ app/src/main/java/com/example/trektopia/utils/Polyline.kt
@@ -1,12 +1,14 @@
 package com.example.trektopia.utils
 
+import android.content.Context
+import com.example.trektopia.R
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.PolyUtil
 
-fun List<LatLng>.getStaticMapUri(): String {
+fun List<LatLng>.getStaticMapUri(context: Context): String {
     val route = PolyUtil.encode(this)
     return "https://maps.googleapis.com/maps/api/staticmap" +
             "?size=400x200" +
             "&path=color:0x0000ff|weight:5|$route" +
-            "&key=${BuildConfig.googleMapsApiKey}"
+            "&key=${context.getString(R.string.google_maps_api_key)}"
 }
