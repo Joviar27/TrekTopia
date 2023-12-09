063078b8141bd4f35f3bf13f92c2ee658a8f9e69
Joviar <joviar27@gmail.com>
2023-12-09 20:37:01 +0700

    Update utils

--- app/src/main/java/com/example/trektopia/utils/DateHelper.kt
+++ app/src/main/java/com/example/trektopia/utils/DateHelper.kt
@@ -1,12 +1,14 @@
 package com.example.trektopia.utils
 
 import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
+import java.util.Date
 import java.util.Locale
 
 object DateHelper{
@@ -31,11 +33,35 @@ object DateHelper{
 
     fun formatDayOfWeek(localDate: LocalDate): String {
         val dayOfWeekFormatter = DateTimeFormatter.ofPattern("EEEE", Locale.ENGLISH)
-        return localDate.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH)
+        return localDate.format(dayOfWeekFormatter)
     }
 
     fun formatDateMonthYear(localDate: LocalDate): String {
         val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ENGLISH)
         return localDate.format(formatter)
+    }
+
+    fun formatElapsedTime(elapsedTimeMillis: Double): String {
+        val totalSeconds = (elapsedTimeMillis / 1000).toLong()
+
+        val hours = totalSeconds / 3600
+        val minutes = (totalSeconds % 3600) / 60
+
+        val formattedString = StringBuilder()
+
+        if (hours > 0) {
+            formattedString.append("$hours ${if (hours == 1L) "Hour" else "Hours"} ")
+        }
+
+        if (minutes > 0) {
+            formattedString.append("$minutes ${if (minutes == 1L) "Minute" else "Minutes"}")
+        }
+
+        return formattedString.toString().trim()
+    }
+
+    fun formatTime(localDateTime: LocalDateTime): String {
+        val formatter = DateTimeFormatter.ofPattern("HH:mm", Locale.ENGLISH)
+        return formatter.format(localDateTime)
     }
 }
