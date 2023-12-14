c3672231c6815753cc57b80c5925a91efb1a6779
Joviar <joviar27@gmail.com>
2023-12-14 17:06:13 +0700

    Set default value
    (cherry picked from commit 14ab1dcf37a340ffcda5bcb71c4a1af56158a6ce)

--- app/src/main/java/com/example/trektopia/core/model/Activity.kt
+++ app/src/main/java/com/example/trektopia/core/model/Activity.kt
@@ -2,17 +2,16 @@ package com.example.trektopia.core.model
 
 import android.os.Parcelable
 import com.google.firebase.Timestamp
-import com.google.android.gms.maps.model.LatLng
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Activity(
-    val id: String,
-    val timeStamp: Timestamp,
-    val duration: Double,
-    val startTime: Timestamp,
-    val stepCount: Int,
-    val distance: Double,
-    val speed: Double,
-    val route: List<LatLng>
+    val id: String = "",
+    val timeStamp: Timestamp = Timestamp.now(),
+    val duration: Double = 0.0,
+    val startTime: Timestamp = Timestamp.now(),
+    val stepCount: Int = 0,
+    val distance: Double = 0.0,
+    val speed: Double = 0.0,
+    val route: String = ""
 ) : Parcelable
