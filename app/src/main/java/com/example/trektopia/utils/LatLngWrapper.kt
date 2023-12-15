6e2ab28f2be0710b721147be5f722a93881468cd
Joviar <joviar27@gmail.com>
2023-12-15 20:55:38 +0700

    Change latlng wrapper data type

--- app/src/main/java/com/example/trektopia/utils/LatLngWrapper.kt
+++ app/src/main/java/com/example/trektopia/utils/LatLngWrapper.kt
@@ -1,12 +1,11 @@
 package com.example.trektopia.utils
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LatLngWrapper(
-    val latLngList: List<LatLng>
+    val latLngList: ArrayList<LatLng>
     ) : Parcelable
 
