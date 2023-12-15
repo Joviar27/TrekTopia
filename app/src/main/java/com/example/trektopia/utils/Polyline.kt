aac6f34214f32ed9c5682639e48c146338c8eb73
Joviar <joviar27@gmail.com>
2023-12-15 21:00:08 +0700

    Update setting for static map request

--- app/src/main/java/com/example/trektopia/utils/Polyline.kt
+++ app/src/main/java/com/example/trektopia/utils/Polyline.kt
@@ -3,21 +3,20 @@ package com.example.trektopia.utils
 import android.content.Context
 import com.example.trektopia.R
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.PolyUtil
 
 fun List<LatLng>.getStaticMapUri(): String {
-    require(this.size <= 5) { "Up to 5 markers are allowed." }
+    require(this.size <= 6) { "Up to 6 markers are allowed." }
 
     val baseUrl = "https://maps.geoapify.com/v1/staticmap"
     val style = "osm-carto"
-    val width = 300
-    val height = 300
-    val zoom = 14
+    val width = 600
+    val height = 600
+    val zoom = 13.8
 
     val center = "lonlat:${this[0].longitude},${this[0].latitude}"
 
-    val markers = this.take(5).mapIndexed { index, latLng ->
-        "lonlat:${latLng.longitude},${latLng.latitude};color:%23ff0000;size:medium;text:${(index).toChar()}"
+    val markers = this.mapIndexed { index, latLng ->
+        "lonlat:${latLng.longitude},${latLng.latitude};color:%23ff0000;size:medium;text:$index"
     }.joinToString("|")
 
     return "$baseUrl?style=$style&width=$width&height=$height&center=$center&zoom=$zoom&marker=$markers"
