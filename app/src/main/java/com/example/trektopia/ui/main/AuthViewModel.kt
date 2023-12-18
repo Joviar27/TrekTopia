9ad843270a93a548804f7d434a2b2b5459e2afb0
Joviar <joviar27@gmail.com>
2023-12-18 14:16:35 +0700

    Add new notif and reset preference function in home and auth viewmodel

--- app/src/main/java/com/example/trektopia/ui/main/AuthViewModel.kt
+++ app/src/main/java/com/example/trektopia/ui/main/AuthViewModel.kt
@@ -3,9 +3,11 @@ package com.example.trektopia.ui.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.trektopia.core.repository.AuthRepository
+import com.example.trektopia.core.repository.Repository
 
 class AuthViewModel(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val repository: Repository
 ) : ViewModel() {
 
     val authState =
@@ -13,5 +15,9 @@ class AuthViewModel(
 
     fun logout() = authRepository.logout()
 
+    fun getNotifStatus() = repository.getNotificationStatus()
+    fun setNotifStatus(status: Boolean) = repository.setNotificationStatus(status)
 
+    fun getResetStatus() = repository.getResetStatus()
+    fun setResetStatus(status: Boolean) = repository.setResetStatus(status)
 }
