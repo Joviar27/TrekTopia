e75c273958d977f5db805b6da67df074853852b2
Joviar <joviar27@gmail.com>
2023-12-18 14:13:46 +0700

    Update status dialog behaviour

--- app/src/main/java/com/example/trektopia/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/trektopia/ui/register/RegisterFragment.kt
@@ -1,8 +1,6 @@
 package com.example.trektopia.ui.register
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +28,7 @@ class RegisterFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _viewModel = this.obtainViewModel()
+        _viewModel = this.obtainViewModel(requireContext())
     }
 
     override fun onCreateView(
