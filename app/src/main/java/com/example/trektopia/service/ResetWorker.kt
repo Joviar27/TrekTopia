14e02ffa17dafef9de0fc900e0f6b5c362cc8056
Joviar <joviar27@gmail.com>
2023-12-18 14:20:49 +0700

    Update check if latest active is previous day, add uid to reset mission in reset worker
    (cherry picked from commit bdd09b941aa67551fc4a548ce417bec6200195c8)

--- app/src/main/java/com/example/trektopia/service/ResetWorker.kt
+++ app/src/main/java/com/example/trektopia/service/ResetWorker.kt
@@ -3,34 +3,27 @@ package com.example.trektopia.service
 import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
-import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.trektopia.core.ResultState
 import com.example.trektopia.core.di.Injection
-import com.example.trektopia.core.repository.AuthRepository
-import com.example.trektopia.core.repository.GameRepository
-import com.example.trektopia.core.repository.Repository
 import com.google.firebase.firestore.FirebaseFirestoreException
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
-import java.time.LocalDate
-import java.time.temporal.ChronoUnit
 
-class FirestoreWorker(
+class ResetWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : CoroutineWorker(context, workerParams) {
 
     private var gameRepository= Injection.provideGameRepository()
-    private var repository= Injection.provideRepository()
+    private var repository= Injection.provideRepository(context)
     private var authRepository= Injection.provideAuthRepository()
 
     override suspend fun doWork(): Result  = coroutineScope{
         try {
             val uid = authRepository.getUid().first()
 
-            repository.checkLatestActiveDate(uid).collect(){result ->
+            repository.checkLatestActivePreviousDate(uid).collect{result ->
                 if(result is ResultState.Success){
                     updateData(uid, result.data)
                 } else if(result is ResultState.Error){
@@ -45,24 +38,18 @@ class FirestoreWorker(
         }
     }
 
-    private suspend fun updateData(uid: String, latestActive: LocalDate){
-        if(!isPreviousDay(latestActive)){
-            gameRepository.resetStreak(uid).collect(){resetStreakResult ->
+    private suspend fun updateData(uid: String, isActiveToday: Boolean){
+        if(!isActiveToday){
+            gameRepository.resetStreak(uid).collect{resetStreakResult ->
                 if(resetStreakResult is ResultState.Error)
                     throw FirebaseFirestoreException(resetStreakResult.error,
                         FirebaseFirestoreException.Code.NOT_FOUND)
             }
         }
-        gameRepository.resetDailyMission().collect(){ resetMissionResult ->
+        gameRepository.resetDailyMission(uid).collect{ resetMissionResult ->
             if(resetMissionResult is ResultState.Error)
                 throw FirebaseFirestoreException(resetMissionResult.error,
                     FirebaseFirestoreException.Code.NOT_FOUND)
         }
-    }
-
-    private fun isPreviousDay(localDate: LocalDate): Boolean {
-        val currentDate = LocalDate.now()
-        val previousDate = currentDate.minus(1, ChronoUnit.DAYS)
-        return localDate.isEqual(previousDate)
     }
 }
